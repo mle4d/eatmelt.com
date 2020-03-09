f8599c75a563db09de2785cd66297c5d58a4168a
emily <mle4d@me.com>
2020-03-09 15:59:28 -0700

    second expand logic for second section

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 document.querySelector('a').addEventListener('click', function() {
   document.querySelector('.menu').classList.toggle('show');
+  window.body
 });
+
+document.querySelector('#openabout').addEventListener('click', function() {
+  document.querySelector('.about').classList.toggle('showabout');
+  window.body
+});
+
